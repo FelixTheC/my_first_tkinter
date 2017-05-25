ff8e0747786c15ecdbc477f0cb140740b78a9e80
FelixTheC <felixeisenmenger@gmx.net>
2017-05-25 13:32:10 +0200

    [UPDATE] make databaseSql more dynamic

--- databaseSql.py
+++ databaseSql.py
@@ -5,38 +5,47 @@ def dbConnect(dbname='lib.db'):
     conn = sqlite3.connect(dbname)
     return conn
 
-def create_table():
-    conn = dbConnect()
+def create_table(dbConnect, table, text):
+    conn = dbConnect
     cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
+    cur.execute("CREATE TABLE IF NOT EXISTS "+ table +" ("+ text +")")
     conn.commit()
     conn.close
 
-def insert(item, quantity, price):
-    conn = dbConnect()
+def insert(dbConnect, table, value1, value2, value3, value4):
+    conn = dbConnect
     cur = conn.cursor()
-    cur.execute('INSERT INTO store VALUES(?,?,?)', (item, quantity,price))
+    cur.execute('INSERT INTO ' + table + ' VALUES(NULL,?,?,?,?)',
+     (value1, value2, value3, value4))
     conn.commit()
     conn.close
 
-def update(quantity, price, item):
-    conn = dbConnect()
+def update(dbConnect, table, id, title, author, year, isbn):
+    conn = dbConnect
     cur = conn.cursor()
-    cur.execute('UPDATE store SET quantity=?, price=? WHERE item=?', (quantity, price, item))
+    cur.execute("UPDATE "+ table +" SET title=?, author=?, year=?, isbn=? WHERE id=?", (title,author,year,isbn,id))
+    conn.commit()
+    conn.close()
+
+def delete(dbConnect, table, id):
+    conn = dbConnect
+    cur = conn.cursor()
+    cur.execute('DELETE FROM '+ table +' WHERE id=?', (id,))
     conn.commit()
     conn.close
 
-def delete(item):
-    conn = dbConnect()
+def search(dbConnect, title="",author="",year="",isbn=""):
+    conn = dbConnect
     cur = conn.cursor()
-    cur.execute('DELETE FROM store WHERE item=?', (item,))
-    conn.commit()
-    conn.close
+    cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?", (title,author,year,isbn))
+    rows = cur.fetchall()
+    conn.close()
+    return rows
 
-def view():
-    conn = dbConnect()
+def view(dbConnect, table):
+    conn = dbConnect
     cur = conn.cursor()
-    cur.execute('SELECT * FROM store')
+    cur.execute('SELECT * FROM '+ table)
     rows = cur.fetchall()
     conn.close
     return rows
